c42a58d0863481ec5ec5e65f8963f8b28840fd83
Thales Eduardo <thalesdev22@gmail.com>
2021-09-25 11:05:01 -0300

    Migração de Merge GraphQL Schemas

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,1 +1,12 @@
-export default {};
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeResolvers } from '@graphql-tools/merge';
+import path from 'path';
+
+const resolversArray = loadFilesSync(
+  path.join(__dirname, '..', 'modules', '**', 'resolvers.js'),
+);
+
+const mergeTypes = mergeResolvers(resolversArray);
+
+console.log(mergeTypes);
+export default mergeTypes;
--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -1,5 +1,11 @@
-export default `
-  type Query {
-    hello: String
-  }
-`;
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+import path from 'path';
+
+const typesArray = loadFilesSync(
+  path.join(__dirname, '..', 'modules', '**', '*.gql'),
+);
+
+const mergeArray = mergeTypeDefs(typesArray);
+console.log(mergeArray);
+export default mergeArray;
